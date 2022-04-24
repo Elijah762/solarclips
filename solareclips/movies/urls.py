e50ac8390266a64e8cca3de81afe089ae8f3d67e
Elijah <stryker1850@gmail.com>
2022-04-23 19:14:09 -0500

    Added navigation across movie app

---
+++ solareclips/movies/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import HomePageView, MoviePageView, MovieDetailView
+
+urlpatterns = [
+  path('', HomePageView.as_view(), name='home'),
+  path('movie/', MoviePageView.as_view(), name='movies'),
+  path('movies/<int:pk>/', MovieDetailView.as_view(), name='movie_detail')
+]
--- solareclips/movies/views.py
+++ solareclips/movies/views.py
@@ -1,3 +1,13 @@
+from re import template
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
+class HomePageView(TemplateView):
+  template_name = 'home_example.html'
+
+class MoviePageView(TemplateView):
+  template_name = 'browse_example.html'
+
+class MovieDetailView(TemplateView):
+  template_name = 'movie_detail_example.html'
 # Create your views here.
