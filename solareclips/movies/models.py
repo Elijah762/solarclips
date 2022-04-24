226b1be4262b337bd6ad4bbcbc11a5a57fa61a20
Elijah <stryker1850@gmail.com>
2022-04-23 19:37:27 -0500

    Added Item model and detailed view path

--- solareclips/movies/models.py
+++ solareclips/movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
+
+
+class Movie(models.Model):
+  title = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.title
+  
+  def get_absolute_path(self):
+    return reverse("movie_detail", kwargs={"pk": self.pk})
 
 # Create your models here.
--- solareclips/movies/urls.py
+++ solareclips/movies/urls.py
@@ -4,5 +4,5 @@ from .views import HomePageView, MoviePageView, MovieDetailView
 urlpatterns = [
   path('', HomePageView.as_view(), name='home'),
   path('movie/', MoviePageView.as_view(), name='movies'),
-  path('movies/<int:pk>/', MovieDetailView.as_view(), name='movie_detail')
+  path('movie/<int:pk>/', MovieDetailView.as_view(), name='movie_detail')
 ]
